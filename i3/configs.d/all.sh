7e784de1fdd4b3f3789add229651c6387baca1f6
Andrew Goacher <andrewgoacher@outlook.com>
2019-03-17 00:01:51 +0000

    Change all script and allow i3 to reset config

--- i3/configs.d/all.sh
+++ i3/configs.d/all.sh
@@ -1,1 +1,5 @@
-cat settings.conf windows.conf gaps.conf bindsym.conf exec.conf > ~/.config/i3/config
+cat ~/.config/i3/configs.d/settings.conf \
+    ~/.config/i3/configs.d/windows.conf \
+    ~/.config/i3/configs.d/gaps.conf \
+    ~/.config/i3/configs.d/bindsym.conf \
+    ~/.config/i3/configs.d/exec.conf > ~/.config/i3/config
